398eb5c91afdcc69b99471e310048e054dd528b5
Kristaps Ritins <kritinsdev@gmail.com>
2023-07-20 00:05:37 +0300

    cron eevery 2 hours

--- app.js
+++ app.js
@@ -2,18 +2,11 @@ const cron = require('node-cron');
 const Scraper = require('./inc/Scraper');
 const config = require('./siteConfig');
 
-function printStartText() {
-    console.log("Scraping is starting...");
-}
-
-// cron.schedule('*/5 * * * *', () => {
-//     printStartText();
-//     startScrape();
-// });
+cron.schedule('0 */2 * * *', () => {
+    startScrape();
+});
 
 async function startScrape() {
     const scraper = new Scraper();
     await scraper.scrape(config);
 }
-
-    startScrape();
