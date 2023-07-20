bc8fec520ede735732a33e35d3fb3262a2314187
Kristaps Ritins <kritinsdev@gmail.com>
2023-07-20 16:33:55 +0300

    dockerfile added

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const cron = require('node-cron');
 const Scraper = require('./inc/Scraper');
 const config = require('./siteConfig');
 
-// cron.schedule('*/2 * * * *', () => {
-//     console.log('Cron job started at', new Date().toLocaleString());
-//     startScrape();
-// });
+cron.schedule('*/5 * * * *', () => {
+    console.log('Cron job started at', new Date().toLocaleString());
+    startScrape();
+});
 
 async function startScrape() {
     const scraper = new Scraper();
     await scraper.scrape(config);
 }
 
-startScrape();
+// startScrape();
